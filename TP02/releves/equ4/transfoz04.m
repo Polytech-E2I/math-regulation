befcaf45cf1da6be68d995b5383c4c4bb551bd37
Robin SHAMSNEJAD <rsham@free.fr>
2022-01-28 09:22:20 +0100

    Remove useless stuff in scripts

--- TP02/releves/equ4/transfoz04.m
+++ TP02/releves/equ4/transfoz04.m
@@ -5,15 +5,15 @@ omega0 = 0.05
 
 for Zeta = Zetas
     Gp = tf(omega0*omega0, [1, 2*Zeta*omega0, omega0*omega0]);
-    
+
     Gzi = c2d(Gp, Ts, 'impulse');
     Gzz = c2d(Gp, Ts, 'zoh');
-    
+
     fprintf("Zeta=%d\n",Zeta);
     Gp
     Gzi
     Gzz
-    
+
     figure()
     impulse(Gzz)
     figure()
